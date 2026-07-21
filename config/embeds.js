fa88e6a12263501a9809ff7276a5f49aa0f46ef3
thegrandcityvaloria <thegrandcityvaloria@gmail.com>
2026-07-21 23:18:24 +0700

    Update embeds.js

--- config/embeds.js
+++ config/embeds.js
@@ -1,1 +1,19 @@
+import { EmbedBuilder } from "discord.js";
 
+export function createEmbed(title) {
+
+    return new EmbedBuilder()
+
+        .setColor("#111111")
+
+        .setTitle(title)
+
+        .setTimestamp()
+
+        .setFooter({
+
+            text: "The Grand City Of Valoria"
+
+        });
+
+}
