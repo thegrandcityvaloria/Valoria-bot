22ad4a787d8bab3ab0fcba96dd120d2e22028f80
thegrandcityvaloria <thegrandcityvaloria@gmail.com>
2026-07-22 20:55:08 +0700

    Create config.js

---
+++ commands/config.js
@@ -0,0 +1,76 @@
+import {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    PermissionFlagsBits
+} from "discord.js";
+
+import ServerConfig from "../models/serverConfig.js";
+
+
+export default {
+
+name: "config",
+
+data: new SlashCommandBuilder()
+    .setName("config")
+    .setDescription("ตั้งค่าระบบ Valoria")
+    .addBooleanOption(option =>
+        option
+        .setName("delete_leave")
+        .setDescription("ลบตัวละครเมื่อผู้เล่นออกจากเซิร์ฟหรือไม่")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+        PermissionFlagsBits.Administrator
+    ),
+
+
+async execute(interaction){
+
+    const value =
+        interaction.options.getBoolean("delete_leave");
+
+
+    await ServerConfig.findOneAndUpdate(
+        {
+            guildId: interaction.guild.id
+        },
+        {
+            deleteOnLeave: value
+        },
+        {
+            upsert:true
+        }
+    );
+
+
+    const embed = new EmbedBuilder()
+
+    .setColor("#111111")
+
+    .setTitle("VALORIA SYSTEM")
+
+    .setDescription(`
+        
+**Server Configuration Updated**
+
+Player Leave Delete:
+${value ? "🗑️ Enabled" : "💾 Disabled"}
+
+━━━━━━━━━━━━━━━━
+
+${value 
+? "เมื่อผู้เล่นออก ตัวละครจะถูกลบ"
+: "ข้อมูลตัวละครจะถูกเก็บไว้"
+}
+`);
+
+
+await interaction.reply({
+    embeds:[embed]
+});
+
+
+}
+
+};
