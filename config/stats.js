1113e0511695a55b33623af08d6f1a289ee12dc4
thegrandcityvaloria <thegrandcityvaloria@gmail.com>
2026-07-22 19:44:04 +0700

    Update stats.js

--- config/stats.js
+++ config/stats.js
@@ -1,82 +1,126 @@
 export const raceStats = {
+
     human: {
+
         hp: 120,
         maxHp: 120,
+
         mp: 80,
         maxMp: 80,
+
         str: 20,
-        dex: 10,
-        agi: 10,
+        dex: 12,
+        agi: 12,
         vit: 15,
         int: 0,
-        luck: 5
+        luck: 10
+
     },
 
     elf: {
+
         hp: 90,
         maxHp: 90,
-        mp: 150,
-        maxMp: 150,
+
+        mp: 160,
+        maxMp: 160,
+
         str: 5,
-        dex: 15,
+        dex: 20,
         agi: 20,
         vit: 8,
-        int: 20,
-        luck: 10
+        int: 25,
+        luck: 15
+
     },
 
     beast: {
+
         hp: 180,
         maxHp: 180,
+
         mp: 40,
         maxMp: 40,
-        str: 25,
+
+        str: 28,
         dex: 15,
         agi: 15,
-        vit: 20,
+        vit: 22,
         int: 0,
         luck: 5
+
     },
 
     demon: {
-        hp: 100,
-        maxHp: 100,
-        mp: 200,
-        maxMp: 200,
-        str: 10,
-        dex: 10,
-        agi: 10,
-        vit: 10,
-        int: 25,
-        luck: 10
+
+        hp: 110,
+        maxHp: 110,
+
+        mp: 180,
+        maxMp: 180,
+
+        str: 15,
+        dex: 12,
+        agi: 18,
+        vit: 12,
+        int: 28,
+        luck: 8
+
     }
+
 };
 
 export const jobStats = {
+
     knight: {
+
+        maxHp: 80,
+
         str: 20,
         vit: 20,
-        hp: 50
+        dex: 5
+
     },
 
     mage: {
+
+        maxMp: 120,
+
         int: 20,
-        mp: 80,
-        vit: 2
+        vit: 2,
+        luck: 5
+
     },
 
     trader: {
+
+        maxHp: 20,
+        maxMp: 20,
+
         luck: 20,
-        dex: 10
+        dex: 10,
+        int: 5
+
     },
 
     chef: {
-        vit: 10,
-        hp: 20
+
+        maxHp: 40,
+
+        vit: 15,
+        luck: 10,
+        dex: 5
+
     },
 
     bartender: {
+
+        maxMp: 40,
+
         agi: 15,
-        luck: 10
+        luck: 15,
+        int: 10
+
     }
+
 };
