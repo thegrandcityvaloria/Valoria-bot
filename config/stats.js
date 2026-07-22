6102fe85f5c541b120db4e6aebb6031fe0d46fb2
thegrandcityvaloria <thegrandcityvaloria@gmail.com>
2026-07-22 19:31:12 +0700

    Create stats.js

---
+++ config/stats.js
@@ -0,0 +1,82 @@
+export const raceStats = {
+    human: {
+        hp: 120,
+        maxHp: 120,
+        mp: 80,
+        maxMp: 80,
+        str: 20,
+        dex: 10,
+        agi: 10,
+        vit: 15,
+        int: 0,
+        luck: 5
+    },
+
+    elf: {
+        hp: 90,
+        maxHp: 90,
+        mp: 150,
+        maxMp: 150,
+        str: 5,
+        dex: 15,
+        agi: 20,
+        vit: 8,
+        int: 20,
+        luck: 10
+    },
+
+    beast: {
+        hp: 180,
+        maxHp: 180,
+        mp: 40,
+        maxMp: 40,
+        str: 25,
+        dex: 15,
+        agi: 15,
+        vit: 20,
+        int: 0,
+        luck: 5
+    },
+
+    demon: {
+        hp: 100,
+        maxHp: 100,
+        mp: 200,
+        maxMp: 200,
+        str: 10,
+        dex: 10,
+        agi: 10,
+        vit: 10,
+        int: 25,
+        luck: 10
+    }
+};
+
+export const jobStats = {
+    knight: {
+        str: 20,
+        vit: 20,
+        hp: 50
+    },
+
+    mage: {
+        int: 20,
+        mp: 80,
+        vit: 2
+    },
+
+    trader: {
+        luck: 20,
+        dex: 10
+    },
+
+    chef: {
+        vit: 10,
+        hp: 20
+    },
+
+    bartender: {
+        agi: 15,
+        luck: 10
+    }
+};
