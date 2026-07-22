91eebcc64abdd19fdc86377b982d0d7612fe0df3
thegrandcityvaloria <thegrandcityvaloria@gmail.com>
2026-07-22 21:52:22 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -28,10 +28,75 @@ const commands = [
         ]
     },
 
-    {
-        name:"register",
-        description:"สร้างตัวละคร"
-    },
+{
+    name:"register",
+    description:"สร้างตัวละครในเมืองวาโลเรีย",
+
+    options:[
+
+        {
+            name:"name",
+            description:"ชื่อตัวละคร",
+            type:3,
+            required:true
+        },
+
+        {
+            name:"race",
+            description:"เลือกเผ่าพันธุ์",
+            type:3,
+            required:true,
+            choices:[
+                {
+                    name:"มนุษย์",
+                    value:"human"
+                },
+                {
+                    name:"เอลฟ์",
+                    value:"elf"
+                },
+                {
+                    name:"สัตว์อสูร",
+                    value:"beast"
+                },
+                {
+                    name:"ปีศาจ",
+                    value:"demon"
+                }
+            ]
+        },
+
+        {
+            name:"job",
+            description:"เลือกอาชีพ",
+            type:3,
+            required:true,
+            choices:[
+                {
+                    name:"อัศวิน",
+                    value:"knight"
+                },
+                {
+                    name:"นักเวท",
+                    value:"mage"
+                },
+                {
+                    name:"พ่อค้า",
+                    value:"trader"
+                },
+                {
+                    name:"เชฟ",
+                    value:"chef"
+                },
+                {
+                    name:"บาร์เทนเดอร์",
+                    value:"bartender"
+                }
+            ]
+        }
+
+    ]
+},
 
     {
         name:"config",
